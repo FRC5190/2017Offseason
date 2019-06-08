ec48a6359212c1dbd4933db5232c9452aede6fce
5190laptop <ghhscomputer@gmail.com>
2019-06-08 12:57:44 -0400

    Changes for 6/8/2019

--- src/main/kotlin/org/ghrobotics/robot/subsytems/drive/DriveSubsystem.kt
+++ src/main/kotlin/org/ghrobotics/robot/subsytems/drive/DriveSubsystem.kt
@@ -7,6 +7,7 @@ package org.ghrobotics.robot.subsytems.drive
 
 import com.ctre.phoenix.motorcontrol.ControlMode
 import com.ctre.phoenix.motorcontrol.NeutralMode
+import edu.wpi.first.wpilibj.SpeedController
 import org.ghrobotics.lib.commands.FalconSubsystem
 import org.ghrobotics.lib.mathematics.units.amp
 import org.ghrobotics.lib.mathematics.units.derivedunits.volt
@@ -30,6 +31,13 @@ object DriveSubsystem : FalconSubsystem() {
     private val rightMotors = arrayOf(rightMaster, rightSlave1)
 
     private val allMotors = arrayOf(*leftMotors, *rightMotors)
+
+    val kDefaultQuickStopThreshold = 0.2
+    val kDefaultQuickStopAlpha = 0.1
+
+    private var m_quickStopThreshold = kDefaultQuickStopThreshold
+    private var m_quickStopAlpha = kDefaultQuickStopAlpha
+    private var m_quickStopAccumulator = 0.0
 
     var lowPowerMode by Delegates.observable(false) { _, _, lowPowerMode ->
         allMotors.forEach {
@@ -76,7 +84,98 @@ object DriveSubsystem : FalconSubsystem() {
     }
 
     fun set(controlMode: ControlMode, leftOutput: Double, rightOutput: Double) {
-        leftMaster.set(controlMode, leftOutput)
-        rightMaster.set(controlMode, rightOutput)
+        leftMaster.set(controlMode, leftOutput * 0.6)
+        rightMaster.set(controlMode, rightOutput * 0.6)
+    }
+
+    fun curvatureDrive(xSpeed: Double, zRotation: Double, isQuickTurn: Boolean) {
+        var xSpeed = xSpeed
+        var zRotation = zRotation
+
+        xSpeed = limit(xSpeed)
+        xSpeed = applyDeadband(xSpeed, 0.02)
+
+        zRotation = limit(zRotation)
+        zRotation = applyDeadband(zRotation, 0.02)
+
+        val angularPower: Double
+        val overPower: Boolean
+
+        if (isQuickTurn) {
+            if (Math.abs(xSpeed) < m_quickStopThreshold) {
+                m_quickStopAccumulator =
+                    (1 - m_quickStopAlpha) * m_quickStopAccumulator + m_quickStopAlpha * limit(zRotation) * 2.0
+            }
+            overPower = true
+            angularPower = zRotation
+        } else {
+            overPower = false
+            angularPower = Math.abs(xSpeed) * zRotation - m_quickStopAccumulator
+
+            if (m_quickStopAccumulator > 1) {
+                m_quickStopAccumulator -= 1.0
+            } else if (m_quickStopAccumulator < -1) {
+                m_quickStopAccumulator += 1.0
+            } else {
+                m_quickStopAccumulator = 0.0
+            }
+        }
+
+        var leftMotorOutput = xSpeed + angularPower
+        var rightMotorOutput = xSpeed - angularPower
+
+        // If rotation is overpowered, reduce both outputs to within acceptable range
+        if (overPower) {
+            if (leftMotorOutput > 1.0) {
+                rightMotorOutput -= leftMotorOutput - 1.0
+                leftMotorOutput = 1.0
+            } else if (rightMotorOutput > 1.0) {
+                leftMotorOutput -= rightMotorOutput - 1.0
+                rightMotorOutput = 1.0
+            } else if (leftMotorOutput < -1.0) {
+                rightMotorOutput -= leftMotorOutput + 1.0
+                leftMotorOutput = -1.0
+            } else if (rightMotorOutput < -1.0) {
+                leftMotorOutput -= rightMotorOutput + 1.0
+                rightMotorOutput = -1.0
+            }
+        }
+
+        // Normalize the wheel speeds
+        val maxMagnitude = Math.max(Math.abs(leftMotorOutput), Math.abs(rightMotorOutput))
+        if (maxMagnitude > 1.0) {
+            leftMotorOutput /= maxMagnitude
+            rightMotorOutput /= maxMagnitude
+        }
+
+        set(ControlMode.PercentOutput, leftMotorOutput, rightMotorOutput)
+    }
+
+    fun limit(value: Double): Double {
+        if (value > 1.0) {
+            return 1.0
+        }
+        return if (value < -1.0) {
+            -1.0
+        } else value
+    }
+
+    /**
+     * Returns 0.0 if the given value is within the specified range around zero. The remaining range
+     * between the deadband and 1.0 is scaled from 0.0 to 1.0.
+     *
+     * @param value    value to clip
+     * @param deadband range around zero
+     */
+    fun applyDeadband(value: Double, deadband: Double): Double {
+        return if (Math.abs(value) > deadband) {
+            if (value > 0.0) {
+                (value - deadband) / (1.0 - deadband)
+            } else {
+                (value + deadband) / (1.0 - deadband)
+            }
+        } else {
+            0.0
+        }
     }
 }
--- src/main/kotlin/org/ghrobotics/robot/subsytems/drive/ManualDriveCommand.kt
+++ src/main/kotlin/org/ghrobotics/robot/subsytems/drive/ManualDriveCommand.kt
@@ -9,7 +9,10 @@ import com.ctre.phoenix.motorcontrol.ControlMode
 import edu.wpi.first.wpilibj.GenericHID
 import org.ghrobotics.lib.commands.FalconCommand
 import org.ghrobotics.lib.utils.withDeadband
+import org.ghrobotics.lib.wrappers.hid.getRawButton
+import org.ghrobotics.lib.wrappers.hid.getX
 import org.ghrobotics.lib.wrappers.hid.getY
+import org.ghrobotics.lib.wrappers.hid.kX
 import org.ghrobotics.robot.Controls
 
 class ManualDriveCommand : FalconCommand(DriveSubsystem) {
@@ -18,13 +21,18 @@ class ManualDriveCommand : FalconCommand(DriveSubsystem) {
         private const val deadband = 0.02
         private val leftSource = Controls.mainXbox.getY(GenericHID.Hand.kLeft).withDeadband(deadband)
         private val rightSource = Controls.mainXbox.getY(GenericHID.Hand.kRight).withDeadband(deadband)
+
+        private val front = Controls.mainXbox.getY(GenericHID.Hand.kLeft).withDeadband(deadband)
+        private val rotation = Controls.mainXbox.getX(GenericHID.Hand.kLeft).withDeadband(deadband)
+        private val quickTurn = Controls.mainXbox.getRawButton(kX)
     }
 
     override suspend fun execute() {
-        DriveSubsystem.set(
-            ControlMode.PercentOutput,
-            -leftSource(),
-            -rightSource()
-        )
+//        DriveSubsystem.set(
+//            ControlMode.PercentOutput,
+//            -leftSource(),
+//            -rightSource()
+//        )
+        DriveSubsystem.curvatureDrive(-front(), rotation(), quickTurn())
     }
 }
